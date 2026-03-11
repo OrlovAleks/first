36e3aefb0ad59561d605535ceef9556176565636
OrlovAleks <Olorgee123@gmail.com>
2026-03-11 10:32:04 +0300

    Update max height for scrollable filters
    Adjusted the max height of the scrollable filters in FilterSidebar.

--- src/components/FilterSidebar.tsx
+++ src/components/FilterSidebar.tsx
@@ -135,7 +135,7 @@ const FilterSidebar = ({ filters, onChange, onApply, resultCount }: FilterSideba
       </div>
 
       {/* Scrollable filters */}
-      <div className="flex-1 overflow-y-auto p-4" style={{ maxHeight: "calc(100vh - 430px)" }}>
+      <div className="flex-1 overflow-y-auto p-4" style={{ maxHeight: "calc(100vh - 330px)" }}>
         <Accordion type="multiple" defaultValue={["price", "brand"]} className="space-y-0">
           {/* Цена */}
           <AccordionItem value="price">
